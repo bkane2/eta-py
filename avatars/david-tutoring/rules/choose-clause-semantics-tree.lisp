4bce8e914fb9a24df54df2ac45ad879dc456b434
bkane2 <Ben.kane@me.com>
2023-07-26 20:23:38 -0400

    Removed ulf-coref directives

--- avatars/david-tutoring/rules/choose-clause-semantics-tree.lisp
+++ avatars/david-tutoring/rules/choose-clause-semantics-tree.lisp
@@ -7,9 +7,9 @@
 (READRULES '*clause-semantics-tree*
 '(
   1 (spatial-question 0)
-    2 (*spatial-question-ulf-tree* (2)) (0 :ulf-coref)
+    2 (*spatial-question-ulf-tree* (2)) (0 :subtree)
   1 (your name is 0)
-    2 (*name-ulf-tree* (1 2 3 4)) (0 :ulf-coref)
+    2 (*name-ulf-tree* (1 2 3 4)) (0 :subtree)
   1 (0 .SPECIAL-REQUEST 0)
-    2 (*request-ulf-tree* (1 2 3)) (0 :ulf-coref)
+    2 (*request-ulf-tree* (1 2 3)) (0 :subtree)
 ))
