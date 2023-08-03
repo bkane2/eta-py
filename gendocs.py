33fd22b43c943ce1293263ed4d864c54e71add1e
bkane2 <Ben.kane@me.com>
2023-08-02 20:41:53 -0400

    Added code for documentation

---
+++ gendocs.py
@@ -0,0 +1,17 @@
+import glob
+import os
+
+"""
+Generate the code documentation, using pydoc.
+# TODO: add recursive packages
+"""
+
+for dirname in ['eta']:
+    flist = glob.glob('%s/*.py' % dirname)
+    for fname in flist:
+        if '__init__' not in fname:
+            os.system('pydoc -w %s' % fname)
+            bname = os.path.splitext(os.path.basename(fname))[0]  # eg 'smartbox'
+            os.system('mv %s.html %s.%s.html' % (bname, dirname, bname))
+    os.system('pydoc -w %s' % dirname)
+os.system('mv *.html docs')
