33ebec1e728b7a1fea72cac163dce652439f3fe7
bkane2 <Ben.kane@me.com>
2023-08-17 12:52:25 -0400

    Updated to read initial Eta utterances

--- terminal.py
+++ terminal.py
@@ -3,10 +3,25 @@ import argparse
 import os
 from time import sleep
 
+def read_eta(fname_in_words, fname_in_affect):
+    with open(fname_in_words, 'r') as f:
+      words = f.read()
+    with open(fname_in_affect, 'r') as f:
+      affect = f.read()
+    os.remove(fname_in_words)
+    os.remove(fname_in_affect)
+    
+    print(f'[{affect}] {words}')
+
+
 def main(args):
   fname_out = f'io/{args.agent_id}/{args.user_id}/in/speech.txt'
   fname_in_words = f'io/{args.agent_id}/{args.user_id}/turn-output.txt'
   fname_in_affect = f'io/{args.agent_id}/{args.user_id}/turn-affect.txt'
+
+  if os.path.isfile(fname_in_words) and os.path.isfile(fname_in_affect):
+    read_eta(fname_in_words, fname_in_affect)
+
   while True:
     sleep(.1)
     utt = input()
@@ -17,16 +32,8 @@ def main(args):
 
     while not os.path.isfile(fname_in_words) or not os.path.isfile(fname_in_affect):
       sleep(.1)
+    read_eta(fname_in_words, fname_in_affect)
     
-    with open(fname_in_words, 'r') as f:
-      words = f.read()
-    with open(fname_in_affect, 'r') as f:
-      affect = f.read()
-    os.remove(fname_in_words)
-    os.remove(fname_in_affect)
-    
-    print(f'[{affect}] {words}')
-
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Eta terminal.')
